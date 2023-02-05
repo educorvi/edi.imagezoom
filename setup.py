1b223c4bf61bcd89d5ba3a5b0f8abef0a51b9000
Lars Walther <lwalther@novareto.de>
2023-02-05 14:20:07 +0100

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='edi.imagezoom',
-    version='1.0a1',
+    version='1.0a2.dev0',
     description="Integrates the js-image-zoom library in Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
